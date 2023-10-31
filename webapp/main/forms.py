1f97c90d261de8c404b3ef602a617b8f355efe48
Ahmed Minhaj <minhajlb19@gmail.com>
2023-10-31 17:57:58 -0400

    signup modified

--- webapp/main/forms.py
+++ webapp/main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(UserCreationForm):
@@ -9,3 +10,8 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email.endswith('@wayne.edu'):
+            raise ValidationError('You must use a wayne.edu email address')
+        return email
--- webapp/main/views.py
+++ webapp/main/views.py
@@ -15,7 +15,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('signin')
         else:
             print(form.errors)
     else:
