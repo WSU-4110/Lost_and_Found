b76e264fa52df6df38939c742b14f0c2ee780782
NajeSafford <Naje.safford@gmail.com>
2023-11-16 19:54:21 -0500

    Update Test_models.py

--- webapp/main/tests/test_models.py
+++ webapp/main/tests/test_models.py
@@ -1,22 +1,71 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from django.utils import timezone
+from unittest import TestCase, mock
+from main.forms import ReportForm
 from main.models import Report
 
-class ReportExistenceTest(TestCase):
+class ReportFormTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='yvaskiv', password='s0Rry/40lera')
-        self.report = Report.objects.create(
-            Name='Test Report',
-            Brand='Test Brand',
-            Location='Test Location',
-            Category='Test Category',
-            description='Test Description',
-            author=self.user,
-            date_created=timezone.now().date()
-        )
+        self.form = ReportForm()
+        self.mock_report = mock.create_autospec(Report, instance=True)
 
-    def test_report_exists(self):
-        date_created = self.report.date_created
-        report_exists = Report.objects.filter(date_created=date_created).exists()
-        self.assertTrue(report_exists)
+    @mock.patch.object(ReportForm, 'save')
+    def test_save(self, mock_save):
+        mock_save.return_value = self.mock_report
+        self.form.save()
+        self.assertTrue(mock_save.called)
+
+    def test_form_valid(self):
+        form = ReportForm(data={
+            'Name': 'Test Report',
+            'Brand': 'Test Brand',
+            'Location': 'Test Location',
+            'Category': 'Test Category',
+            'description': 'Test Description',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_form_invalid(self):
+        form = ReportForm(data={
+            'Name': 'Test Report',
+            'Brand': 'Test Brand',
+            'Location': 'Test Location',
+            'Category': 'Test Category',
+            'description': '',
+        })
+        self.assertFalse(form.is_valid())   
+
+    def test_form_invalid2(self):
+        form = ReportForm(data={
+            'Name': 'Test Report',
+            'Brand': 'Test Brand1',
+            'Location': 'Test Location',
+            'Category': '',
+            'description': 'Test Description',
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_form_invalid3(self):        
+        form = ReportForm(data={
+            'Name': 'Test Report',
+            'Brand': '',
+            'Location': 'Test Location',
+            'Category': 'Test Category',
+            'description': 'Test Description',
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_form_invalid4(self):
+        form = ReportForm(data={
+            'Name': '',
+            'Brand': 'Test Brand',
+            'Location': 'Test Location',
+            'Category': 'Test Category',
+            'description': 'Test Description',
+        })
+        self.assertFalse(form.is_valid())
+
+
+
+
+
+
+
