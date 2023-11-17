272c959bb7aa7cf8d6074f654c73b63330817311
Yuliya Vaskiv <juliavaskiv001@gmail.com>
2023-11-17 18:24:06 -0500

    patch testing

--- webapp/main/tests/test_models.py
+++ webapp/main/tests/test_models.py
@@ -16,7 +16,54 @@ class ReportExistenceTest(TestCase):
             date_created=timezone.now().date()
         )
 
-    def test_report_exists(self):
+    def test_report_exists_by_category(self):
+        category = self.report.Category
+        report_exists = Report.objects.filter(Category=category).exists()
+        self.assertTrue(report_exists)
+
+
+    def test_report_exists_by_date(self):
         date_created = self.report.date_created
         report_exists = Report.objects.filter(date_created=date_created).exists()
         self.assertTrue(report_exists)
+
+    def test_report_exists_by_name(self):
+        name = self.report.Name
+        report_exists = Report.objects.filter(Name=name).exists()
+        self.assertTrue(report_exists)
+
+    def test_report_exists_by_brand(self):
+        brand = self.report.Brand
+        report_exists = Report.objects.filter(Brand=brand).exists()
+        self.assertTrue(report_exists)
+
+    def test_report_exists_by_location(self):
+        location = self.report.Location
+        report_exists = Report.objects.filter(Location=location).exists()
+        self.assertTrue(report_exists)
+
+    def test_report_exists_by_category(self):
+        category = self.report.Category
+        report_exists = Report.objects.filter(Category=category).exists()
+        self.assertTrue(report_exists)
+
+    def test_report_exists_by_description(self):
+        description = self.report.description
+        report_exists = Report.objects.filter(description=description).exists()
+        self.assertTrue(report_exists)
+    
+    def test_report_exists_by_author(self):
+        author = self.report.author
+        report_exists = Report.objects.filter(author=author).exists()
+        self.assertTrue(report_exists)
+
+    def test_report_exists_by_date_created(self):
+        date_created = self.report.date_created
+        report_exists = Report.objects.filter(date_created=date_created).exists()
+        self.assertTrue(report_exists)
+
+    def test_report_does_not_exist_by_name(self):
+        name = 'Non-existent Report'
+        report_exists = Report.objects.filter(Name=name).exists()
+        self.assertFalse(report_exists)
+
