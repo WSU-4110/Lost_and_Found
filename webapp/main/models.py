d2986920bc6a1e3b2646308d9c20e739b4a9fb5f
Yuliya Vaskiv <juliavaskiv001@gmail.com>
2023-11-22 15:10:17 -0500

    rearranging
    menu changed
    list post and create post is combined
    new list report page with delete options

--- webapp/main/models.py
+++ webapp/main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 # Create your models here.
 
 class Post(models.Model):
@@ -22,8 +24,14 @@ class Report(models.Model):
     date_updated= models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image_link = models.URLField(null=True)
+    is_resolved = models.BooleanField(default=False)
     def __str__(self):
         return self.Name + "\n" + self.description
+    def save(self, *args, **kwargs):
+        if self.pk is not None:  # the report has been saved before
+            self.date_updated = timezone.now()
+        super().save(*args, **kwargs)
+
 
 '''
 class ChatRoom(models.Model):
