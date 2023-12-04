00213fed7d70bb7dc3d700e2b494607c1193c7cd
Ahmed Minhaj <74390236+AhmedMinhajEmon@users.noreply.github.com>
2023-12-04 18:49:58 -0500

    Update urls.py

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     #path('fetch_messages/<int:chat_room_id>', views.fetch_messages, name='fetch_messages'),
     path('create_report/', views.create_report, name='create_report'),
     path('list_report/', views.list_report, name='list_report'),
+    path('upload/', views.image_upload, name='upload'),
     path('delete_report/<int:report_id>/', views.delete_report, name='delete_report'),
 ]
