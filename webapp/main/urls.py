f6b302f8f07b7f531eda7ceb90426dd16066b422
Ahmed Minhaj <74390236+AhmedMinhajEmon@users.noreply.github.com>
2023-12-10 15:46:22 -0500

    Update urls.py

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.list_report, name='list_report'),
-    path('home/', views.home, name='home'),
+    path('home', views.home, name='home'),
     # add more URL patterns here
     path('create_post/', views.create_post, name='create_post'),
    # path('home/', views.search_posts, name='search_posts'),
